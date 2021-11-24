8854e00bd2f3569bcb906bfae18742eb8d6a5e41
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2021-11-24 16:35:21 +0800

    Add the click handler for start button & display data

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -26,10 +26,10 @@ data class SleepNight(
     var nightId: Long = 0L,
 
     @ColumnInfo(name = "start_time_milli")
-    val startTimeMillli: Long = System.currentTimeMillis(),
+    val startTimeMilli: Long = System.currentTimeMillis(),
 
     @ColumnInfo(name = "end_time_milli")
-    var endTimeMilli: Long = startTimeMillli,
+    var endTimeMilli: Long = startTimeMilli,
 
     @ColumnInfo(name = "quality_rating")
     var sleepQuality: Int = -1
